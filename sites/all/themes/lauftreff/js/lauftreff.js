e4f9c58a2392795cab91ed6d15e988083bb2baf5
Thomas Moseler <tmoseler@gmx.de>
2017-03-11 12:46:46 +0100

    Rezie-Script für die Kontaktbox auch bei window.resize ausführen.

--- sites/all/themes/lauftreff/js/lauftreff.js
+++ sites/all/themes/lauftreff/js/lauftreff.js
@@ -3,8 +3,17 @@
   // Funktioniert dann auch bei Ajax Reloads.
   Drupal.behaviors.lauftreff = {
     attach: function (context, settings) {
-      var height_left = $('.front .front-left').outerHeight(true) + $('.front h1#page-title').outerHeight(true);
-      $('.front .front-right').height(height_left);
+
+      function resize_contact_front() {
+        var height_left = $('.front .front-left').outerHeight(true) + $('.front h1#page-title').outerHeight(true);
+        $('.front .front-right').height(height_left);
+        console.log(height_left);
+      };
+
+      resize_contact_front();
+      $(window).resize(function(){
+        resize_contact_front();
+      });
 
     } // end of attach function
   };
--- sites/all/themes/lauftreff/templates/page.tpl.php
+++ sites/all/themes/lauftreff/templates/page.tpl.php
@@ -4,7 +4,7 @@
 
       <?php if ($logo): ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print url(path_to_theme() . '/images/lykia-logo.gif'); ?>" alt="<?php print t('Home'); ?>" />
+          <img src="<?php print $logo ?>" alt="<?php print t('Home'); ?>" />
         </a>
       <?php endif; ?>
 
